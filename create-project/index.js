bac91a9599cb7fc88bd237254f9fcd5b031082e6
pheliml <pj.lennon@hotmail.com>
2025-03-26 13:13:02 +0000

    feat add golang project

--- create-project/index.js
+++ create-project/index.js
@@ -28,12 +28,45 @@ function createJsProj(projectStructure) {
 	});
 
 
-	console.log("Project structure created successfully!");
+	console.log("Javascript project structure created successfully!");
+}
+
+function createGoProj(projectStructure) {
+
+	if (argv.src && argv.src != "main")
+	{
+		console.log("override with main file");
+	}
+
+	projectStructure = {
+		'cmd': ['main.go'], //cmd instead of src
+		'internal': ['app.go'], //files in internal dirs are private
+		'pkg': ['public.go'], //files in pkg dirs are public
+		'test': ['main_test.go'], //test file for the main application
+		'scripts': ['script.go']
+		}
+	
+	Object.entries(projectStructure).forEach(([dir, files]) => {
+	fs.mkdirSync(dir, { recursive: true });
+	files.forEach(file => fs.writeFileSync(`${dir}/${file}`, ''));
+	});
+
+	console.log("Go project structure created successfully!");
 }
 
 if (argv.lang)
 {
-	createJsProj(projectStructure)
+	switch (argv.lang)
+	{
+		case "js":
+			createJsProj(projectStructure)
+			break;
+		case "go":
+			createGoProj(projectStructure)
+			break;
+		default:
+			break;
+	}
 } else {
 	console.log("argument --lang required");
 }
