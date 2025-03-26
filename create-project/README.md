b85d9dad77a157ce39ab610cba70ad29a81b9548
pheliml <pj.lennon@hotmail.com>
2025-03-26 12:32:40 +0000

    func: accept cli args | chore: add readme & gitignore

---
+++ create-project/README.md
@@ -0,0 +1,5 @@
+env: node@22.14.0
+
+cmd: create-project
+
+desc: Creates a specific project structure within a directory
--- create-project/index.js
+++ create-project/index.js
@@ -1,16 +1,30 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-const projectStructure = {
-	'src': ['index.js'],
-	'public': ['index.html', 'styles.css']
-};
+let projectStructure = {}
+
+if(argv.src)
+{
+	projectStructure = {
+		'src': [`${argv.src}.js`],
+		'public': ['index.html', 'styles.css']
+	}
+} else {
+
+	projectStructure = {
+		'src': ['index.js'],
+		'public': ['index.html', 'styles.css']
+	}
+}
 
 Object.entries(projectStructure).forEach(([dir, files]) => {
-	fs.mkdirSync(dir, { recursive: true });
-	files.forEach(file => fs.writeFileSync(`${dir}/${file}`, ''));
+fs.mkdirSync(dir, { recursive: true });
+files.forEach(file => fs.writeFileSync(`${dir}/${file}`, ''));
 });
+
 
 console.log("Project structure created successfully!");
 
