61751ae1a5eabe5b307928e64749f01f345517ed
phelim@LNX-SY016 <pj.lennon@hotmail.com>
2025-06-19 15:25:45 +0100

    init.lua

--- config/init.lua
+++ config/init.lua
@@ -1,29 +1,90 @@
--- Load packer (required)
+-- init.lua
+-- zM close all folds
+-- zR open all folds
+-- zc/zo open/close fold under cursor
+
+-- Load packer
 vim.cmd [[packadd packer.nvim]]
 
--- Use packer to manage plugins
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'       -- Packer manages itself
+  use 'wbthomason/packer.nvim' -- Packer manages itself
 
-  -- Add plugins here, for example:
-  use {
-    'ibhagwan/fzf-lua',
-    requires = { 'nvim-tree/nvim-web-devicons' }
+-- PLUGINS {{{
+--
+  -- fuzzy finder written in lua {{{
+  use 
+  {
+	  'ibhagwan/fzf-lua',
+	  requires = { 'nvim-tree/nvim-web-devicons' }
   }
-end)
+  require('fzf-lua').setup
+  {
+	  files = {
+		  -- Use fd as the file finder and ignore node_modules and .git
+		  fd_opts = "--color=never --type f --hidden --exclude node_modules --exclude .git"
+	  }
+  }
+  -- }}}
 
+  -- enable floating terminal in nvim {{{
+  use 
+  {
+	  "akinsho/toggleterm.nvim", tag = '*', config = function()
+		  require("toggleterm").setup
+		  {
+			  size = 20,
+			  open_mapping = [[<c-\>]], -- Ctrl+\ to toggle terminal
+    		  shading_factor = 2,
+    		  direction = 'float', -- makes the terminal float
+    		  float_opts = { border = 'curved' }
+  		  }
+    end
+  }
+  -- }}}
+
+  -- monokai-pro colorscheme {{{
+  use 
+  {
+	  "loctvl842/monokai-pro.nvim",
+	  config = function()
+		  require("monokai-pro").setup()
+	end
+  }
+  -- }}}
+
+  -- treesitter for advanced syntax highlighting {{{
+  use 
+  {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = ':TSUpdate'
+  }
+  require'nvim-treesitter.configs'.setup 
+  {
+	  ensure_installed = { "java", "lua", "typescript", "go" },
+	  highlight = {
+		  enable = true, -- Enable Treesitter-based highlighting
+		  additional_vim_regex_highlighting = false,  -- Disable traditional highlighting
+  	  },
+  }
+  -- }}}
+
+  end
+)
+-- }}}
+
+-- CORE EDITOR CONFIG {{{
+--
 -- Disable compatibility with old vi
 vim.opt.compatible = false
 
--- map leader to " "
+-- map leader key to " "
 vim.g.mapleader = ' '
 
  --Enable syntax highlighting
 vim.cmd("syntax on")
 
--- Set color scheme and background
-vim.opt.background = "dark"
-vim.cmd("colorscheme unokai")
+-- Set color scheme
+vim.cmd([[colorscheme monokai-pro]])
 
 -- Line numbers
 vim.opt.number = true
@@ -38,19 +99,21 @@ vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+-- Fold marker
+vim.opt.foldenable = true
+vim.opt.foldlevel = 0
+vim.opt.foldmethod = "marker"
+
+-- }}}
+
+-- KEY MAPS {{{
+-- 
 -- Key mapping: Ctrl+x Ctrl+j to convert keys to JSON-style quoted strings
 vim.api.nvim_set_keymap("n", "<C-x><C-j>", [[:%s/[ \t]\([A-Za-z_].*\):/"\1":<CR>]], { noremap = true, silent = true })
-
-require('fzf-lua').setup({
-  files = {
-    -- Use fd as the file finder and ignore node_modules and .git
-    fd_opts = "--color=never --type f --hidden --exclude node_modules --exclude .git"
-  }
-})
-
 vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true })
+-- source lua on space rl
+vim.api.nvim_set_keymap('n', '<leader>rl', ':luafile ~/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true })
 
--- source lua on \
-vim.api.nvim_set_keymap('n', '<leader>r', ':luafile ~/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
-
+-- }}}
