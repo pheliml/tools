183f7f3c00bcaf9a201f4c685e3faff0622623f2
phelim@LNX-SY016 <pj.lennon@hotmail.com>
2025-07-21 16:39:48 +0100

    fix neovim install message

--- config/nvim-upgrade.sh
+++ config/nvim-upgrade.sh
@@ -8,5 +8,5 @@ tmp=$(mktemp)
 curl -L https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage -o "$tmp"
 chmod +x "$tmp"
 sudo install -Dm755 "$tmp" /usr/local/bin/nvim
-echo "✅ Neovim installed at /usr/local/bin/nvim → $(nvim --version | head -1)"
+echo "Neovim installed at /usr/local/bin/nvim → $(nvim --version | head -1)"
 
